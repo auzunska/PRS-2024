c4c284306a1e1a51998ad4b025e1a62b3a8a9d9a
auzunska <auzunska@abv.bg>
2024-10-26 13:24:44 +0300

    Lab3 init

---
+++ PRS-Lab3-REST/PRS-Lab3/PRS_Lab3/PRS_Lab3-Server/IService1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PRS_Lab3_Server
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Addit?num1={num1}&num2={num2}",
+            Method = "GET",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Xml
+            )]
+        string AddIt(int num1, int num2);
+    }
+}
---
+++ PRS-Lab3-REST/PRS-Lab3/PRS_Lab3/PRS_Lab3-Server/Service1.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace PRS_Lab3_Server
+{
+    public class Service1 : IService1
+    {
+        private readonly string xmlFilePath = "D:/TU_Sem_7/PRS/PRS-2024/PRS-Lab3-REST/PRS-Lab3/data.xml";
+        public string AddIt(int num1, int num2)
+        {
+            return Convert.ToString(num1 + num2);
+        }
+    }
+}
